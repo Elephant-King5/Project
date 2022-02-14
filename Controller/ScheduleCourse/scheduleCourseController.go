3864c87b66dd3f5f9d6cb12cb948322b9eebdf99
oceanct <yangzhichao_hit@163.com>
2022-02-14 11:25:19 +0800

    Schedule Course COntroller

---
+++ Controller/ScheduleCourse/scheduleCourseController.go
@@ -0,0 +1,21 @@
+package ScheduleCourse
+
+import (
+	"Project1/Service/ScheduleCourse"
+	"Project1/Types"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type ScheCourseController struct {
+}
+
+func (s ScheCourseController) ScheduleCourse(c *gin.Context) {
+	scheduleCourseRequest := &Types.ScheduleCourseRequest{}
+	if err := c.ShouldBindJSON(scheduleCourseRequest); err != nil {
+		c.JSON(http.StatusOK, gin.H{"error": err.Error()})
+		return
+	}
+	scheduleCourseResponse := ScheduleCourse.Schedule(scheduleCourseRequest)
+	c.JSON(http.StatusOK, &scheduleCourseResponse)
+}
